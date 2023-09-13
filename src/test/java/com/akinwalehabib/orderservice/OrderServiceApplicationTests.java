1bd7836042e70d338644ce29ef79d020514d6caa
Akinwale Habib <akinwalehabib@hotmail.com>
2023-09-13 22:05:44 +0100

    update OrderServiceApplicationTests

--- src/test/java/com/akinwalehabib/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/akinwalehabib/orderservice/OrderServiceApplicationTests.java
@@ -1,13 +1,16 @@
 package com.akinwalehabib.orderservice;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
