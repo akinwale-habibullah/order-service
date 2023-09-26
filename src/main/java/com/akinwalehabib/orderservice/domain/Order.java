3b5c1e7d4aad901ee0c91c17c2616158bf34ec6f
Akinwale Habib <akinwalehabib@hotmail.com>
2023-09-26 19:44:02 +0100

    add security, data conf, testcontainers, and tests

--- src/main/java/com/akinwalehabib/orderservice/domain/Order.java
+++ src/main/java/com/akinwalehabib/orderservice/domain/Order.java
@@ -2,8 +2,10 @@ package com.akinwalehabib.orderservice.domain;
 
 import java.time.Instant;
 
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.relational.core.mapping.Table;
@@ -26,6 +28,12 @@ public record Order (
   @LastModifiedDate
   Instant lastModifiedDate,
 
+  @CreatedBy
+  String createdBy,
+
+  @LastModifiedBy
+  String lastModifiedBy,
+
   @Version
   int version
 ) {
@@ -36,7 +44,7 @@ public record Order (
     Integer quantity,
     OrderStatus status
   ) {
-    return new Order(null, bookIsbn, bookName, bookPrice, quantity, status, null, null, 0);
+    return new Order(null, bookIsbn, bookName, bookPrice, quantity, status, null, null, null, null, 0);
   }
 }
   
