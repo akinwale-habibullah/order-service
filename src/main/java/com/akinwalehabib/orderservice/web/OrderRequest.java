9684531ba7f87a983a88561e9866563afb6c4d87
Akinwale Habib <akinwalehabib@hotmail.com>
2023-10-23 10:44:15 +0100

    fix failing tests

--- src/main/java/com/akinwalehabib/orderservice/web/OrderRequest.java
+++ src/main/java/com/akinwalehabib/orderservice/web/OrderRequest.java
@@ -7,11 +7,11 @@ import javax.validation.constraints.NotNull;
 
 public record OrderRequest(
 
-  @NotBlank
+  @NotBlank(message = "The book ISBN must be defined.")
   String isbn,
 
   @NotNull(message = "The book quantity must be defined.")
-  @Min(value = 1, message = "You must order at least one item.")
+  @Min(value = 1, message = "You must order at least 1 item.")
   @Max(value = 5, message = "You cannot order more than 5 items.")
   Integer quantity
 ) {}
